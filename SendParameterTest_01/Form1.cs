54eb8515f8e237f0a6b84250b82df984465a9e73
Deny Kan <Deny Kan>
2018-02-05 15:36:30 +0900

    초기 커밋.

---
+++ SendParameterTest_01/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using SHDocVw;
+
+namespace SendParameterTest_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 브라우저 객체 생성
+            InternetExplorer ie = new InternetExplorer();
+            ie.Visible = true;
+
+            // 접근할 URL
+            dynamic url = "http://www.google.co.kr";
+
+            // 해당 URL 탐색(브라우저 실행)
+            ie.Navigate2(ref url);
+
+            // 브라우저가 Busy 상태(켜지고 있을 때)일 경우 켜질때까지 기다림
+            while(ie.Busy)
+            {
+                Thread.Sleep(1000);
+            }
+   
+            // 브라우저 객체가 표시하고 있는 HTML페이지를 로드
+            mshtml.HTMLDocument page = ie.Document;
+
+            // 로드한 HTML페이지에 있는 Element중
+            // 1. q라는 id를 가진 Element를 획득 (getElementById("q")
+            // 2. 해당 Element의 value 값을 test1234로 변경 ( .setAttribute("value", "test1234) );
+            page.getElementById("q").setAttribute("value", "신현우는젓가락동맹의저항을받을것이다");
+        }
+
+    }
+}
